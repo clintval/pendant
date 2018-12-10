0ca79bd76476dfb9a9d6a8ac32801b244df5a4c3
clintval <valentine.clint@gmail.com>
2018-12-10 11:30:19 -0800

    Version bump to v0.4.1

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ author = 'clintval'
 # The short X.Y version
 version = '0.4'
 # The full version, including alpha/beta/rc tags
-release = '0.4.0'
+release = '0.4.1'
 
 # -- General configuration ---------------------------------------------------
 
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 from pathlib import Path
 
 PACKAGE = 'pendant'
-VERSION = '0.4.0'
+VERSION = '0.4.1'
 
 setuptools.setup(
     name=PACKAGE,
